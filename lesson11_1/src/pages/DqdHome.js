726a3a86664524892cb8b3b0503206a13ef550ae
Quốc Đạt <166087439+qdat24@users.noreply.github.com>
2025-09-24 23:55:44 +0700

    Delete lesson11_1 directory

--- lesson11_1/src/pages/DqdHome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function DqdHome() {
-  return (
-    <div className='alert alert-success'>
-      <h1 className='text-center'>Welcome to, Quốc Đạt</h1>
-    </div>
-  )
-}
