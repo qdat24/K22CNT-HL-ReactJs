612f8d4169c04260414d8676ed2951eba6a6ae98
qdat24 <quocdat30075@gmail.com>
2025-09-24 23:55:04 +0700

    add new lesson11

---
+++ lesson11/src/pages/DqdHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DqdHome() {
+  return (
+    <div className='alert alert-success'>
+      <h1 className='text-center'>Welcome to, Quốc Đạt</h1>
+    </div>
+  )
+}
